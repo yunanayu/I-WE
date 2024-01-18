c7af124e6231471d22fd1860e6ecf48c337c5c99
wus22 <seojihyeon99@naver.com>
2024-01-18 23:17:13 +0900

    [chore] BE 환경설정 세팅

--- backend/src/main/java/com/iandwe/BackendApplication.java
+++ backend/src/main/java/com/iandwe/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.iandwe.backend;
+package com.iandwe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/iandwe/BackendApplicationTests.java
+++ backend/src/test/java/com/iandwe/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.iandwe.backend;
+package com.iandwe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
