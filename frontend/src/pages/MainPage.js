cdbe8209b4215af859d1d3d64dd1afc6fbe08eea
전승혜 <jfn022@gmail.com>
2024-01-24 17:52:51 +0900

    [feat] 홈화면 로고삽입

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import BasicLayout from "../layouts/BasicLayout";
 import Basic from "../components/navbar/Basic";
 import icon from "../images/icon.png";
+import logo from "../images/logo.png";
 import { Box, Typography } from '@mui/material';
 import GoogleLogin from "./GoogleLoginPage";
 import KakaoLogin from "./KakaoLoginPage";
@@ -18,24 +19,30 @@ const Main = () => {
         </BasicLayout>
       ) : (
         <>
-          
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <img src={icon} alt="icon" style={{ width: '80px', height: '80px' }} />
-            
-            <Typography variant="h4" align="center" sx={{ mt: 4, mb: 2, color: 'gray' }}>
-              Login
-            </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: 8, padding: 4, width: '50%' }}>
-              <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
-                <div style={{ flex: 1 }}>
-                  <KakaoLogin sx={{ width: '100%' }} />
-                </div>
-                <div style={{ flex: 1 }}>
-                  <NaverLogin sx={{ width: '100%' }} />
-                </div>
-                <div style={{ flex: 1 }}>
-                  <GoogleLogin sx={{ width: '100%' }} />
-                </div>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-bottom', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+              <img src={icon} alt="icon" style={{ width: '300px', height: '270px' }} />
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-bottom', gap: 2 }}>
+                  <Typography variant="h5" align="center" sx={{ mt: 4, mb: 2, color: 'gray' }}>
+                    육아의 모든 것
+                  </Typography>
+                </Box>
+                <img src={logo} alt="logo" style={{ width: '200px', height: '100px' }} />
+              </Box>
+
+              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: 8, padding: 4, width: '50%' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+                  <div style={{ flex: 1 }}>
+                    <KakaoLogin sx={{ width: '100%' }} />
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <NaverLogin sx={{ width: '100%' }} />
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <GoogleLogin sx={{ width: '100%' }} />
+                  </div>
+                </Box>
               </Box>
             </Box>
           </Box>
