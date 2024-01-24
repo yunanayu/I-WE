da98cedc62991001e679662191f340f7b1c88a54
예준성 <306yyy@naver.com>
2024-01-24 17:49:41 +0900

    [feat] 필수 검사/검진/접종 여부 엔티티 작성

---
+++ backend/src/main/java/com/iandwe/checker/domain/BabyChecker.java
@@ -0,0 +1,33 @@
+package com.iandwe.checker.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BabyChecker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long num;
+
+    private Long babyNum;
+
+    private Long essentialNum;
+
+    private boolean complete;
+
+    public void updateComplete(boolean complete){
+        this.complete = complete;
+    }
+
+}
