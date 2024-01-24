c077f1e26b21426cf91f7f57de19e595f55ed994
예준성 <306yyy@naver.com>
2024-01-24 10:48:10 +0900

    [feat] 필수 검진, 검사, 접종 정보 API 구현
     - 아이, 산모, 전체별 필수 정보 조회
     - 시기에 따른 필수 정보 조회

---
+++ backend/src/main/java/com/iandwe/essential/controller/EssentialController.java
@@ -0,0 +1,32 @@
+package com.iandwe.essential.controller;
+
+import com.iandwe.essential.dto.EssentialResponseDto;
+import com.iandwe.essential.service.EssentialService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/essential")
+public class EssentialController {
+
+    private final EssentialService essentialService;
+
+    @GetMapping("/{target}")
+    public ResponseEntity<List<EssentialResponseDto>> findByTarget(@PathVariable String target){
+        return new ResponseEntity<>(essentialService.findByTarget(target), HttpStatus.OK);
+    }
+
+    @GetMapping("/time/{targetTime}")
+    public ResponseEntity<List<EssentialResponseDto>> findByTargetTime(@PathVariable int targetTime) {
+        return new ResponseEntity<>(essentialService.findByTargetTime(targetTime), HttpStatus.OK);
+    }
+
+}
