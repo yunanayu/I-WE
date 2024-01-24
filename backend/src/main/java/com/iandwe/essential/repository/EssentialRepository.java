dc6ef912908e373c536131df83bf464618b9eec0
예준성 <306yyy@naver.com>
2024-01-24 17:51:36 +0900

    [feat] pk num 에 따른 조회 메소드 추가 및 주석 제거

--- backend/src/main/java/com/iandwe/essential/repository/EssentialRepository.java
+++ backend/src/main/java/com/iandwe/essential/repository/EssentialRepository.java
@@ -4,15 +4,14 @@ import com.iandwe.essential.domain.Essential;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EssentialRepository extends JpaRepository<Essential, Long> {
 
-    // 1. 아이의 필수 검사 정보 조회
-    // 2. 산모의 필수 검사 정보 조회
-    // 3. 모든 필수 검사 정보 조회
     List<Essential> findByTarget(String target);
-    // 4. 시기별 검사 정보 조회
 
     List<Essential> findByTargetTime(int targetTime);
+
+    Optional<Essential> findByNum(long num);
 
 }
