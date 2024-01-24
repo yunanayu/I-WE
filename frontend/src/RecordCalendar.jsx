977e85ce7225e95b57a406a0bca719c90b870c74
정유나 <jyn7077@naver.com>
2024-01-24 17:39:09 +0900

    [feat] 캘린더 스타일 커스텀

--- frontend/src/RecordCalendar.jsx
+++ frontend/src/RecordCalendar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './customCalendarStyle.css'
 import moment from 'moment'
 
 const RecordCalendar = () => {
