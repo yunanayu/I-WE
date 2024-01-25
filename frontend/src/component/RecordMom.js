9a637e174df9599970fefd474b3485bd6cec049f
김무준 <power649@naver.com>
2024-01-25 16:12:45 +0900

    [feat] 엄마 기록 컴포넌트 배치완료

--- frontend/src/component/RecordMom.js
+++ frontend/src/component/RecordMom.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import {ChangeChart, WeeklyWeightChart} from './MomWeightChart';
+import { ChangeChart, WeeklyWeightChart } from "./MomWeightChart";
+import { MomForm } from "./WeightForm";
+import { Typography } from "@mui/material";
 
 // 주차별 몸무게
 const weightWeekly = [];
@@ -10,16 +12,16 @@ const weightAvg = null;
 // 임신 주차
 const pregWeek = null;
 // status 구하는 함수 필요
-const status = 'avg';
+const status = "avg";
 // 입력받을 오늘 몸무게
 const weightToday = null;
 
 // 띄워줄 메세지
 const infoMessage = [
-	{stat : 'low', msg: '평균보다 낮습니다'},
-	{stat : 'avg', msg: '평균치입니다'},
-	{stat : 'high', msg: '평균보다 높습니다'},
-]
+  { stat: "low", msg: "평균보다 낮습니다" },
+  { stat: "avg", msg: "평균치입니다" },
+  { stat: "high", msg: "평균보다 높습니다" },
+];
 
 // 가운데 정렬 css
 const setCenter = {
@@ -40,24 +42,47 @@ const commonStyles = {
 
 // low, avg, high에 맞춰서 메세지 출력
 function Info() {
-	const text = infoMessage.find((element) => {
-		if(element.stat === status) return true;
-	});
-	return (
-		<>
-		{text.msg}
-		</>
-	);
+  const text = infoMessage.find((element) => {
+    if (element.stat === status) return true;
+  });
+  return (
+    <>
+      <Box sx={{ mt: 3, mb: 3 }}>
+        <Typography fontSize={34}> {text.msg} </Typography>
+      </Box>
+    </>
+  );
 }
 
+// props 설정, form 전송 객체, 차트 데이터 입력 필요 
 function RecordMom() {
   return (
     <>
       <Container maxWidth="lg" sx={{ ...setCenter, background: "pink" }}>
-        <Box maxWidth="md" sx={{ ...commonStyles, ...setCenter, borderRadius: 3 }}>{<Info />}</Box>
-        <Box maxWidth="md" sx={{ ...commonStyles, ...setCenter, borderRadius: 3 }}><WeeklyWeightChart/></Box>
-        <Box maxWidth="md" sx={{ ...commonStyles, ...setCenter, borderRadius: 3 }}><ChangeChart /></Box>
-        <Box maxWidth="md" sx={{ ...commonStyles, ...setCenter, borderRadius: 3 }}>123</Box>
+        <Box
+          maxWidth="md"
+          sx={{ ...commonStyles, ...setCenter, borderRadius: 3 }}
+        >
+          {<Info />}
+        </Box>
+        <Box
+          maxWidth="md"
+          sx={{ ...commonStyles, ...setCenter, borderRadius: 3 }}
+        >
+          <MomForm />
+        </Box>
+        <Box
+          maxWidth="md"
+          sx={{ ...commonStyles, ...setCenter, borderRadius: 3 }}
+        >
+          <WeeklyWeightChart />
+        </Box>
+        <Box
+          maxWidth="md"
+          sx={{ ...commonStyles, ...setCenter, borderRadius: 3 }}
+        >
+          <ChangeChart />
+        </Box>
       </Container>
     </>
   );
