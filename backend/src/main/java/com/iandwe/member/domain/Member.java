e5fcb4aa9c26135f4be6138876fc8c6f860bcc01
서지현 <seojihyeon99@naver.com>
2024-01-25 09:57:30 +0900

    [feat] Member 도메인 생성

--- backend/src/main/java/com/iandwe/member/domain/Member.java
+++ backend/src/main/java/com/iandwe/member/domain/Member.java
@@ -1,4 +1,35 @@
 package com.iandwe.member.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long num;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String role; //ROLE_USER, ROLE_ADMIN
+
+    @CreationTimestamp
+    private Timestamp createDate;
+//    private String provider; // google
+//    private String providerId; // sub=111114578118192654593
+
+
+
 }
---
+++ backend/src/main/java/com/iandwe/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.iandwe.member.repository;
+
+import com.iandwe.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
