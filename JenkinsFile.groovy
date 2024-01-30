cecc337f55a913351e310a406df34173cd5b7f1c
서지현 <seojihyeon99@naver.com>
2024-01-30 12:05:45 +0900

    [fix] jenkinsfile 수정

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,11 +1,39 @@
 pipeline {
     agent any
-
+    tools {
+      gradle 'gradle_7.5.1'
+    }
     stages {
+        stage('GitLab Repository Clone') {
+            steps {
+                git branch : 'develop', credentialsId: 'SSAFYC108', url: 'https://lab.ssafy.com/s10-webmobile1-sub2/S10P12C108.git'
+            }
+            post {
+                failure {
+                  echo 'Repository clone failure !'
+                }
+                success {
+                  echo 'Repository clone success !'
+                }
+            }
+        }
         stage('Build') {
             steps {
                 echo 'Building..'
             }
+            // steps {
+            //     echo 'Building..'
+            //     sh 'chmod +x gradlew'
+            //     sh './gradlew bootjar'
+            // }
+            // post {
+            //     failure {
+            //         echo 'Gradle jar build failure !'
+            //     }
+            //     success {
+            //         echo 'Gradle jar build success !'
+            //     }                
+            // }
         }
         stage('Test') {
             steps {
