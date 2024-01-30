fc060b5dbfef1f4390e9aab1e72457c6725ff0a3
서지현 <seojihyeon99@naver.com>
2024-01-30 21:22:26 +0900

    [fix] Jenkins 설정파일 수정

--- backend/JenkinsFile.groovy
+++ backend/JenkinsFile.groovy
@@ -6,7 +6,7 @@ pipeline {
     environment {
         DOCKER_IMAGE_NAME = 'jihyeon99/iandwe-backend'
         DOCKERFILE_PATH = './backend/Dockerfile'
-        // CONTAINER_NAME = 'iandwe-backend'
+        CONTAINER_NAME = 'iandwe-backend'
     }
     stages {
         stage('GitLab Clone') {
@@ -56,7 +56,40 @@ pipeline {
                     echo 'Docker Build success !'
                 }
             }
-        }        
+        }
+        stage('Delete Previous Docker Container') {
+            steps {
+                script {
+                    sh '''
+                        docker stop ${CONTAINER_NAME}
+                        docker rm ${CONTAINER_NAME}
+                    '''
+                }
+            }
+            post {
+                failure {
+                    echo 'Delete Previous Docker Container failure !'
+                }
+                success {
+                    echo 'Delete Previous Docker Container success !'
+                }
+            }
+        }
+        stage('Run Docker Container') {
+            steps {
+                script {
+                    sh 'docker run -d --name ${CONTAINER_NAME} -p 3000:3000 ${DOCKER_IMAGE_NAME}'
+                }
+            }
+            post {
+                failure {
+                    echo 'Run Docker Container failure !'
+                }
+                success {
+                    echo 'Run Docker Containersuccess !'
+                }
+            }
+        }
         stage('Test') {
             steps {
                 echo 'Testing..'
