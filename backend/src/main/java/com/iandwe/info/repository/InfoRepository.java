2b77c0f316eda0c0820aa37149b8fa7d766e84f8
윤지현 <goon03077@gmail.com>
2024-02-02 13:34:32 +0900

    [feat] 정보 제공 리팩토링

--- backend/src/main/java/com/iandwe/info/repository/InfoRepository.java
+++ backend/src/main/java/com/iandwe/info/repository/InfoRepository.java
@@ -10,12 +10,17 @@ import java.util.List;
 
 public interface InfoRepository extends JpaRepository<Info, Long> {
 
-//    @Query("SELECT i FROM Info i WHERE i.target = :#{#dto.target} AND i.category = :#{#dto.category}")
+//    @Query("SELECT i FROM Info i " +
+//            "WHERE i.target = :#{#dto.target} " +
+//                "AND i.category = :#{#dto.category} " +
+//                "AND CAST(substring(i.startTime, 2) AS Integer) <= CAST(substring(:#{#dto.targetTime}, 2) AS Integer) " +
+//                "AND CAST(substring(:#{#dto.targetTime}, 2) AS Integer) <= CAST(substring(i.endTime, 2) AS Integer) ")
     @Query("SELECT i FROM Info i " +
             "WHERE i.target = :#{#dto.target} " +
-                "AND i.category = :#{#dto.category} " +
-                "AND substring(i.startTime, 1, 3) <= substring(:#{#dto.targetTime},1, 3) " +
-                "AND substring(i.endTime,1 ,3) >= substring(:#{#dto.targetTime},1, 3) ")
+            "AND i.category = :#{#dto.category} " +
+            "AND substring(i.startTime, 1, 1) = substring(:#{#dto.targetTime}, 1, 1)" +
+            "AND CAST(substring(i.startTime, 2) AS Integer) <= CAST(substring(:#{#dto.targetTime}, 2) AS Integer) " +
+            "AND CAST(substring(:#{#dto.targetTime}, 2) AS Integer) <= CAST(substring(i.endTime, 2) AS Integer) ")
     List<Info> findByTargetAndTargetTimeAndCategory(@Param("dto")InfoRequestDto dto);
 
     String findByNum(Long num);
