221a54dd1afd18caf99e79e59340deb727a0e168
예준성 <306yyy@naver.com>
2024-02-05 17:30:33 +0900

    [refactor] 아이 공유 로직 변경에 따른 baby 도메인 리팩토링

--- backend/src/main/java/com/iandwe/baby/service/BabyServiceImpl.java
+++ backend/src/main/java/com/iandwe/baby/service/BabyServiceImpl.java
@@ -5,6 +5,7 @@ import com.iandwe.baby.dto.*;
 import com.iandwe.baby.exception.NoBabyExistException;
 import com.iandwe.baby.repository.BabyRepository;
 import com.iandwe.checker.service.generator.CheckerGenerator;
+import com.iandwe.family.service.FamilyService;
 import com.iandwe.member.domain.Member;
 import com.iandwe.member.domain.ParentType;
 import com.iandwe.member.repository.MemberRepository;
@@ -24,19 +25,29 @@ public class BabyServiceImpl implements BabyService {
 
     private final MemberRepository memberRepository;
 
+    private final FamilyService familyService;
+
     @Transactional
     @Override
     public BabyCreateResponseDto create(BabyCreateRequestDto dto) {
         Baby baby = dto.toEntity();
 
-        babyRepository.save(baby);
-        checkerGenerator.generateBabyCheckerData(baby.getNum());
-
         Member savedMember = memberRepository.findByNum(dto.getMotherNum()).orElseThrow();
+
+        if (savedMember.getFamilyNum() != null) { // 존재 할 때
+            baby.share(familyService.findFatherByNum(savedMember.getFamilyNum()));
+        } else {
+            familyService.create(savedMember.getNum());
+        }
+
+        babyRepository.save(baby);
 
         if (isMother(savedMember.getParentType())) {
             checkerGenerator.generateMotherCheckerData(savedMember.getNum(), baby.getNum());
+            // family 에서 num 으로 조회 시 familyNum 이 없을 때, family 만들어주고, 있으면 거기서 fathernum도 baby에 가져와서 넣어준다
         }
+
+        checkerGenerator.generateBabyCheckerData(baby.getNum());
 
         return BabyCreateResponseDto.from(baby);
     }
@@ -50,14 +61,6 @@ public class BabyServiceImpl implements BabyService {
         return babies.stream()
                 .map(BabyReadResponseDto::from)
                 .toList();
-    }
-
-    @Override
-    @Transactional
-    public void share(BabyShareRequestDto dto) {
-        Baby baby = babyRepository.findByNum(dto.getBabyNum())
-                .orElseThrow(NoBabyExistException::new);
-        baby.share(dto.getFatherNum());
     }
 
     @Override
