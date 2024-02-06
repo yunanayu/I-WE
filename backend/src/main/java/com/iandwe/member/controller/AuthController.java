87fcf502fa80f075cec794b20de0768e3d346282
서지현 <seojihyeon99@naver.com>
2024-02-07 00:03:24 +0900

    [feat] AuthController 추가

---
+++ backend/src/main/java/com/iandwe/member/controller/AuthController.java
@@ -0,0 +1,49 @@
+package com.iandwe.member.controller;
+
+//import com.adoptpet.server.commons.notification.repository.EmitterRepository;
+//import com.adoptpet.server.commons.security.dto.RefreshToken;
+//import com.adoptpet.server.commons.security.service.JwtUtil;
+//import com.adoptpet.server.commons.support.StatusResponseDto;
+//import com.adoptpet.server.commons.util.SecurityUtils;
+//import com.adoptpet.server.user.dto.response.TokenResponseStatus;
+//import com.adoptpet.server.user.repository.RefreshTokenRepository;
+//import com.adoptpet.server.user.service.RefreshTokenService;
+import com.iandwe.security.SecurityUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final RefreshTokenService tokenService;
+
+    @PostMapping("/token/logout")
+    public ResponseEntity<?> logout(@RequestHeader(value = "Authorization") final String accessToken) {
+        // 엑세스 토큰으로 현재 Redis 정보 삭제
+        tokenService.removeRefreshToken(accessToken, SecurityUtils.getUser());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/token/refresh")
+    public ResponseEntity<?> refresh(@RequestHeader("Authorization") final String accessToken) {
+
+        String newAccessToken = tokenService.republishAccessToken(accessToken);
+        if (StringUtils.hasText(newAccessToken)) {
+            return new ResponseEntity<>(newAccessToken, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.valueOf(400));
+    }
+
+}
