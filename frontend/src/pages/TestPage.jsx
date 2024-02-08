ca9afac4c76ca17c5ada0fe7f4e8d0f156770d53
전승혜 <jfn022@gmail.com>
2024-02-08 13:14:19 +0900

    [feat] 정보 3개씩만 불러오기 및 모달창에 모든정보 띄우기

--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,190 +1,65 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
+export default function ScrollDialog() {
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState('paper');
 
-// function AddChild({ setSpouseStatus }) {
-//   const navigate = useNavigate();
-//   const [childName, setChildName] = useState("");
-//   const [pregnancyDate, setPregnancyDate] = useState("");
-//   const [childGender, setChildGender] = useState("");
-//   const [pregnancyStatus, setPregnancyStatus] = useState("");
-//   const [birthDate, setBirthDate] = useState("");
-//   const setBabyList = useMemberStore(state => state.setBabyList)
-//   const setUserNum = useMemberStore(state => state.setUserNum)
-//   const setParentType = useMemberStore(state => state.setParentType)
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-//   const handleChildNameChange = (event) => {
-//     setChildName(event.target.value);
-//   };
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
 
-//   const handlePregnancyDateChange = (date) => {
-//     const formattedDate = dayjs(date.$d).format("YYYY-MM-DD");
-//     setPregnancyDate(formattedDate);
-//   };
-
-//   const handleBirthDateChange = (date) => {
-//     const formattedDate = dayjs(date.$d).format("YYYY-MM-DD");
-//     setBirthDate(formattedDate);
-//   };
-
-//   const handleChildGenderChange = (event) => {
-//     setChildGender(event.target.value);
-//   };
-
-//   const handlePregnancyStatusChange = (event) => {
-//     setPregnancyStatus(event.target.value);
-//   };
-  
-//   const handleAddChild = async () => {
-//     const cookieString = document.cookie;
-//     const cookies = cookieString.split('; ');
-
-//     var code;
-//     for (const cookie of cookies) {
-//       const [cookieName, cookieValue] = cookie.split('=');
-//       if (cookieName === 'token') {
-//         code = cookieValue;
-//       }
-//     }
-
-//     var userNum;
-//     var parentType
-//     // 사용자 정보 요청해서 Authorization에 넣기
-//     try {
-//       const response = await axios.get('/api/member',
-//         {
-//           headers: {
-//             'Authorization' : code
-//           }
-//         }
-//       );
-//       userNum = response.data.num;
-//       parentType = response.data.parentType;
-//     } catch(e) {
-//       console.log("회원정보 받아오기 실패")
-//     }
-//     // user type, pk 저장
-//     setUserNum(userNum)
-//     setParentType(parentType)
-    
-//     const requestBaby = {
-//       motherNum: userNum, // 해당 유저의 num
-//       name: childName,
-//       gender: childGender === "MALE" ? 1 : childGender === "FEMALE" ? 2 : 0, // 남자=1 여자=2 모름=0
-//       birth: new Date(birthDate), // 아이의 생일
-//       pregnancyDate: new Date(pregnancyDate), // 임신 날짜
-//       status: pregnancyStatus === "pregnancy" ? true : false, // 임신 상태인지 여부를 true 또는 false로 설정
-//     };
-
-//     var babyList = []
-//     // 아기정보 post
-//     try {
-//       const response = await axios.post(`/api/baby`, requestBaby, // requestBaby 정보 전달
-//       {
-//           headers: {
-//             'Authorization' : code
-//           }
-//         }
-//       );
-//       // console.log(response.data);
-//       const babyInfo = response.data
-//       babyList = response.data
-
-//     } catch(e) {
-//       console.log("아기정보 등록 실패")
-//     }
-//     setBabyList(...babyList)
-//     console.log("아기정보 등록 성공")
-//     navigate("/");
-//   };
-
-  
-//   return (
-//     <div>
-//       <div>
-//         <FormControl>
-//           <FormLabel>현재 상태</FormLabel>
-//           <RadioGroup
-//             name="pregnancy-status"
-//             value={pregnancyStatus}
-//             onChange={handlePregnancyStatusChange}
-//           >
-//             <FormControlLabel value="pregnancy" control={<Radio />} label="임신" />
-//             <FormControlLabel value="nonpragnancy" control={<Radio />} label="출산" />
-//           </RadioGroup>
-//         </FormControl>
-//       </div>
-
-//       {pregnancyStatus === "pregnancy" ? (
-//         <>
-//           <TextField
-//             label="아이의 이름"
-//             value={childName}
-//             onChange={handleChildNameChange}
-//           />
-//           <FormControl>
-//             <FormLabel>성별</FormLabel>
-//             <RadioGroup
-//               name="child-gender"
-//               value={childGender}
-//               onChange={handleChildGenderChange}
-//             >
-//               <FormControlLabel value="NONE" control={<Radio />} label="모름" />
-//               <FormControlLabel value="MALE" control={<Radio />} label="남자" />
-//               <FormControlLabel value="FEMALE" control={<Radio />} label="여자" />
-//             </RadioGroup>
-//           </FormControl>
-//           <div>
-//             <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
-//               <DemoContainer components={['DatePicker']}>
-//                 <DesktopDatePicker  
-//                   label="임신 추측일 (마지막 생리 날짜)" 
-//                   value={pregnancyDate}
-//                   onChange={handlePregnancyDateChange}
-//                   shouldDisableDate={(day) => {
-//                     return dayjs(day).isAfter(dayjs(), 'day');
-//                   }}/>
-//               </DemoContainer>
-//             </LocalizationProvider>
-//           </div>
-          
-//         </>
-//       ) : pregnancyStatus === "nonpragnancy" ? (
-//         <>
-//           <FormControl>
-//             <FormLabel>성별</FormLabel>
-//             <RadioGroup
-//               name="child-gender"
-//               value={childGender}
-//               onChange={handleChildGenderChange}
-//             >
-//               <FormControlLabel value="MALE" control={<Radio />} label="남자" />
-//               <FormControlLabel value="FEMALE" control={<Radio />} label="여자" />
-//             </RadioGroup>
-//           </FormControl>
-//           <TextField
-//             label="아이의 이름"
-//             value={childName}
-//             onChange={handleChildNameChange}
-//           />
-//           <div>
-//             <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
-//               <DemoContainer components={['DesktopDatePicker ']}>
-//                 <DesktopDatePicker  
-//                   label="출산일" 
-//                   value={birthDate}
-//                   onChange={handleBirthDateChange}
-//                   shouldDisableDate={(day) => {
-//                     return dayjs(day).isAfter(dayjs(), 'day');
-//                   }}/>
-//               </DemoContainer>
-//             </LocalizationProvider>
-//           </div>
-//         </>
-//       ) : null}
-
-//       <Button variant="contained" onClick={handleAddChild}>아이 추가</Button>
-//     </div>
-//   );
-// }
-
-// export default AddChild;
+  return (
+    <React.Fragment>
+      <Button onClick={handleClickOpen('paper')}>scroll=paper</Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">Subscribe</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            {[...new Array(50)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+              )
+              .join('\n')}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
